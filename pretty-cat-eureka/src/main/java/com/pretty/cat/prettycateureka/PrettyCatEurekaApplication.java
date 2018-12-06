e7b59227b0372944837b2dbea4710737292a8fbc
root <gogs@gogs.com>
2018-12-06 18:02:21 +0800

    1

---
+++ pretty-cat-eureka/src/main/java/com/pretty/cat/prettycateureka/PrettyCatEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.pretty.cat.prettycateureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrettyCatEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrettyCatEurekaApplication.class, args);
+	}
+}
---
+++ pretty-cat-eureka/src/test/java/com/pretty/cat/prettycateureka/PrettyCatEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pretty.cat.prettycateureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrettyCatEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
