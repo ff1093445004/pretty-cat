e3d1c0dd3e29cf3ae5a0f1a196bb7e579aaa380e
root <gogs@gogs.com>
2018-12-10 17:09:47 +0800

    1

---
+++ pretty-cat-bus/src/main/java/com/pretty/cat/prettycatbus/PrettyCatBusApplication.java
@@ -0,0 +1,12 @@
+package com.pretty.cat.prettycatbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrettyCatBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrettyCatBusApplication.class, args);
+	}
+}
