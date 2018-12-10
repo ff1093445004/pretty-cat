65babccc2df498cff89870fd6670d220cd2ac63b
root <gogs@gogs.com>
2018-12-10 14:46:54 +0800

    1

--- pretty-cat-es/src/test/java/com/pretty/cat/prettycates/PrettyCatEsApplicationTests.java
+++ pretty-cat-es/src/test/java/com/pretty/cat/prettycates/PrettyCatEsApplicationTests.java
@@ -2,15 +2,21 @@ package com.pretty.cat.prettycates;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PrettyCatEsApplicationTests {
 
+	@Value("${jdbc.url}")
+	private String value;
+	
 	@Test
 	public void contextLoads() {
+		System.out.println(value);
 	}
 
 }
