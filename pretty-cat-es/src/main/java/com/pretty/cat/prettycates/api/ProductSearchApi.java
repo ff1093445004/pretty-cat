104a2eb6832a4423b01a077e5d6b3463a51ddf1f
root <gogs@gogs.com>
2018-12-10 16:29:33 +0800

    1

--- pretty-cat-es/src/main/java/com/pretty/cat/prettycates/api/ProductSearchApi.java
+++ pretty-cat-es/src/main/java/com/pretty/cat/prettycates/api/ProductSearchApi.java
@@ -5,12 +5,14 @@ import com.pretty.cat.prettycatapi.model.BaseResult;
 import com.pretty.cat.prettycates.service.ProductSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ProductSearchApi implements ProductFeignClient {
 
 	@Autowired
