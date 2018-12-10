fb2d981c8856cc3fa468f3282f980a038d2c7436
root <gogs@gogs.com>
2018-12-10 15:12:48 +0800

    1

--- pretty-cat-es/src/test/java/com/pretty/cat/prettycates/PrettyCatEsApplicationTests.java
+++ pretty-cat-es/src/test/java/com/pretty/cat/prettycates/PrettyCatEsApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@RefreshScope
 public class PrettyCatEsApplicationTests {
 
 	@Value("${zhm.num}")
