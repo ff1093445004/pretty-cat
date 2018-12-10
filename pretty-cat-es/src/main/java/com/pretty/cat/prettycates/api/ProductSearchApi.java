8c3663fc356e7f5291ef89ad978e33d93a08538c
root <gogs@gogs.com>
2018-12-10 16:04:04 +0800

    1

--- pretty-cat-es/src/main/java/com/pretty/cat/prettycates/api/ProductSearchApi.java
+++ pretty-cat-es/src/main/java/com/pretty/cat/prettycates/api/ProductSearchApi.java
@@ -4,6 +4,7 @@ import com.pretty.cat.prettycatapi.api.ProductFeignClient;
 import com.pretty.cat.prettycatapi.model.BaseResult;
 import com.pretty.cat.prettycates.service.ProductSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,5 +20,13 @@ public class ProductSearchApi implements ProductFeignClient {
 	public BaseResult findByName(@RequestParam("page") int page, @RequestParam("size") int size,
 			@RequestParam("name") String name) {
 		return productSearchService.findByNameHighLight(page, size, name);
+	}
+
+	@Value("${zhm.num}")
+	private String value;
+	
+	@RequestMapping(value = "/getValue")
+	public String getValue(){
+		return value;
 	}
 }
