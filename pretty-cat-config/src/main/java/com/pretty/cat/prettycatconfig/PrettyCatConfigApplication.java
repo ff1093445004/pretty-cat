68f5f02131400e24d019748d17ab78e10a9ed40e
root <gogs@gogs.com>
2018-12-10 10:28:36 +0800

    1

---
+++ pretty-cat-config/src/main/java/com/pretty/cat/prettycatconfig/PrettyCatConfigApplication.java
@@ -0,0 +1,14 @@
+package com.pretty.cat.prettycatconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class PrettyCatConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrettyCatConfigApplication.class, args);
+	}
+}
